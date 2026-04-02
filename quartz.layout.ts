a129fe100b122ba2290207a8580c3e869039150b
bendwild <dewildebenjamin0@gmail.com>
2026-04-02 13:28:57 -0700

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,6 +23,10 @@ export const defaultContentPageLayout: PageLayout = {
       component: Component.Breadcrumbs(),
       condition: (page) => page.fileData.slug !== "index",
     }),
+    Component.ConditionalRender({
+  component: Component.NoteHeatmap(),
+  condition: (page) => page.fileData.slug === "index",
+    }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
