be5fd1f7c3573c4349e5bc941b4c93c75aec4602
bendwild <dewildebenjamin0@gmail.com>
2026-04-04 21:47:50 -0700

    Update NoteHeatmap.tsx

--- quartz/components/NoteHeatmap.tsx
+++ quartz/components/NoteHeatmap.tsx
@@ -22,7 +22,7 @@ NoteHeatmap.afterDOMLoaded = `
   const root = document.getElementById("note-heatmap-root")
   if (!root) return
 
-  const heatmapUrl = new URL("static/note-heatmap.json", document.baseURI).toString()
+  const heatmapUrl = "/obsidian/static/note-heatmap.json"
   console.log("Heatmap URL:", heatmapUrl)
 
   fetch(heatmapUrl)
