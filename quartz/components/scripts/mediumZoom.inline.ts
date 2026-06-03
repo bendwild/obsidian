c8cfc967f37fd1d5afd48def053d80e726004996
bendwild <dewildebenjamin0@gmail.com>
2026-06-03 11:27:48 +0200

    Update mediumZoom.inline.ts

--- quartz/components/scripts/mediumZoom.inline.ts
+++ quartz/components/scripts/mediumZoom.inline.ts
@@ -1,13 +1,12 @@
 import mediumZoom from "medium-zoom"
 
-let zoom: ReturnType<typeof mediumZoom> | undefined
-
-const setup = () => {
-  zoom?.detach()
-  zoom = mediumZoom("article img", {
-    margin: 24,
-    background: "rgba(0, 0, 0, 0.85)",
-  })
+const options = {
+  margin: 24,
+  background: "rgba(0, 0, 0, 0.85)",
 }
 
-document.addEventListener("nav", setup)
+const attachZoom = () => {
+  mediumZoom("article img", options)
+}
+
+document.addEventListener("nav", attachZoom)
